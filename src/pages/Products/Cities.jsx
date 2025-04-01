b2ba71fac52945a0b36abda399bf91db8316e1fb
naimovdavlatshoh <you@example.com>
2025-04-01 18:58:11 +0500

    added diferent cities

--- src/pages/Products/Cities.jsx
+++ src/pages/Products/Cities.jsx
@@ -6,16 +6,16 @@ import {
     DialogBody,
     DialogFooter,
 } from "@material-tailwind/react";
-import { AiOutlineCloudUpload } from "react-icons/ai";
 import { FaCity } from "react-icons/fa";
-import { BsCheckCircle, BsXCircle } from "react-icons/bs"; // For the true/false icons
+import { BsCheckCircle, BsXCircle } from "react-icons/bs";
 import { GetDataSimple, PostDataTokenJson } from "../../service";
 import { toast, Toaster } from "react-hot-toast";
 
 export function Cities({ changeStatus, food }) {
+    console.log(food);
     const [open, setOpen] = useState(false);
     const [cities, setCities] = useState([]);
-    const [selectedCity, setSelectedCity] = useState(null); // State to track selected city
+    const [selectedCity, setSelectedCity] = useState(null);
     const lang = localStorage.getItem("lang");
 
     useEffect(() => {
@@ -25,11 +25,12 @@ export function Cities({ changeStatus, food }) {
     }, []);
 
     const handleOpen = () => {
-        setOpen(!open), setSelectedCity(null);
+        setOpen(!open);
+        setSelectedCity(null);
     };
 
     const handleCityClick = (city) => {
-        setSelectedCity(city.id === selectedCity ? null : city.id); // Toggle the selected city
+        setSelectedCity(city.id === selectedCity ? null : city.id);
     };
 
     const addProduct = () => {
@@ -41,23 +42,20 @@ export function Cities({ changeStatus, food }) {
                     changeStatus();
                     setOpen(false);
                     setSelectedCity(null);
-                    toast.success(
-                        localStorage.getItem("lang") == "ru"
-                            ? "Добавлено"
-                            : "Qo'shildi"
-                    );
+                    toast.success(lang === "ru" ? "Добавлено" : "Qo'shildi");
                 })
                 .catch(() => {
                     setOpen(false);
                     setSelectedCity(null);
                     toast.error(
-                        localStorage.getItem("lang") == "ru"
+                        lang === "ru"
                             ? "Что-то пошло не так!"
                             : "Nimadir xato ketdi!"
                     );
                 });
         }
     };
+
     const removeProduct = () => {
         if (selectedCity) {
             PostDataTokenJson(`api/product/delcity/${food.product_id}`, {
@@ -67,17 +65,13 @@ export function Cities({ changeStatus, food }) {
                     changeStatus();
                     setOpen(false);
                     setSelectedCity(null);
-                    toast.success(
-                        localStorage.getItem("lang") == "ru"
-                            ? "Добавлено"
-                            : "Qo'shildi"
-                    );
+                    toast.success(lang === "ru" ? "Удалено" : "O'chirildi");
                 })
-                .catch((err) => {
+                .catch(() => {
                     setOpen(false);
                     setSelectedCity(null);
                     toast.error(
-                        localStorage.getItem("lang") == "ru"
+                        lang === "ru"
                             ? "Что-то пошло не так!"
                             : "Nimadir xato ketdi!"
                     );
@@ -107,35 +101,61 @@ export function Cities({ changeStatus, food }) {
                     className="space-y-4 p-4 bg-gray-100 rounded-lg overflow-y-auto max-h-[50vh]"
                 >
                     <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                        {cities.map((city) => (
-                            <div
-                                key={city.id}
-                                className="bg-white rounded-lg shadow-md hover:shadow-xl transition-shadow py-4 flex flex-col items-center cursor-pointer"
-                                onClick={() => handleCityClick(city)} // Click handler
-                            >
-                                <FaCity size={24} className="text-main mb-2" />
-                                <span className="font-medium text-xs">
-                                    {lang === "ru"
-                                        ? city?.city_name_ru
-                                        : city?.city_name_uz}
-                                </span>
+                        {cities.map((city) => {
+                            const isSelected = selectedCity === city.id;
+                            const isFoodCity = food?.cities?.some(
+                                (c) => c.city_id === city.id
+                            );
 
-                                {selectedCity === city.id && ( // Show icons only if city is selected
-                                    <div className="mt-2 flex space-x-4">
-                                        <BsCheckCircle
-                                            onClick={() => addProduct()}
-                                            size={20}
-                                            className="text-green-500 cursor-pointer"
-                                        />
-                                        <BsXCircle
-                                            onClick={() => removeProduct()}
-                                            size={20}
-                                            className="text-red-500 cursor-pointer"
-                                        />
-                                    </div>
-                                )}
-                            </div>
-                        ))}
+                            return (
+                                <div
+                                    key={city.id}
+                                    className={`${
+                                        isFoodCity
+                                            ? "bg-main text-white"
+                                            : "bg-white "
+                                    } rounded-lg shadow-md hover:shadow-xl transition-shadow py-4 flex flex-col items-center cursor-pointer`}
+                                    onClick={() => handleCityClick(city)}
+                                >
+                                    <FaCity
+                                        size={24}
+                                        className={`mb-2 ${
+                                            isFoodCity
+                                                ? "text-white"
+                                                : "text-main"
+                                        }`}
+                                    />
+                                    <span
+                                        className={`font-medium text-xs ${
+                                            isFoodCity ? "text-white" : ""
+                                        }`}
+                                    >
+                                        {lang === "ru"
+                                            ? city?.city_name_ru
+                                            : city?.city_name_uz}
+                                    </span>
+
+                                    {isSelected && (
+                                        <div className="mt-2 flex space-x-4">
+                                            <BsCheckCircle
+                                                onClick={() => addProduct()}
+                                                size={20}
+                                                className="text-green-500 cursor-pointer"
+                                            />
+                                            <BsXCircle
+                                                onClick={() => removeProduct()}
+                                                size={20}
+                                                className={`${
+                                                    isFoodCity
+                                                        ? "text-white"
+                                                        : "text-main"
+                                                } cursor-pointer`}
+                                            />
+                                        </div>
+                                    )}
+                                </div>
+                            );
+                        })}
                     </div>
                 </DialogBody>
                 <DialogFooter className="bg-gray-100 rounded-b-lg">
